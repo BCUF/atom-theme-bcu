0c83c02f77185f085f0f12292b4adc8e5af43423
sborne <sylvain.borne@fr.ch>
2021-09-07 16:24:09 +0200

    Update _box.php
    test button with icon

--- modules/search/templates/_box.php
+++ modules/search/templates/_box.php
@@ -13,7 +13,7 @@
       <?php endif; ?>
 
 <!-- Emplacement bouton dropdown -->
-  <button type="submit" class="btn">Search</button>
+  <button type="submit" class="btn"><i class="icon-search"></i></button>
 
     </div>
 
