3d1d883849b12b10578d6c58c1321520f782699a
sborne <sylvain.borne@fr.ch>
2021-09-07 16:07:45 +0200

    Update _box.php
    test remove recherche générale

--- modules/search/templates/_box.php
+++ modules/search/templates/_box.php
@@ -14,44 +14,11 @@
 
 <!-- Emplacement bouton dropdown -->
 
-
-
     </div>
 
     <div id="search-realm" class="search-popover">
 
-      <?php if (sfConfig::get('app_multi_repository')): ?>
 
-        <div>
-          <label>
-            <?php if (isset($repository)): ?>
-              <input name="repos" type="radio" value data-placeholder="<?php echo __('Search') ?>">
-            <?php else: ?>
-              <input name="repos" type="radio" value checked="checked" data-placeholder="<?php echo __('Search') ?>">
-            <?php endif; ?>
-            <?php echo __('Global search') ?>
-          </label>
-        </div>
-
-        <?php if (isset($repository)): ?>
-          <div>
-            <label>
-              <input name="repos" checked="checked" type="radio" value="<?php echo $repository->id ?>"/>
-              <?php echo __('Search <span>%1%</span>', array('%1%' => render_title($repository))) ?>
-            </label>
-          </div>
-        <?php endif; ?>
-
-        <?php if (isset($altRepository)): ?>
-          <div>
-            <label>
-              <input name="repos" type="radio" value="<?php echo $altRepository->id ?>"/>
-              <?php echo __('Search <span>%1%</span>', array('%1%' => render_title($altRepository))) ?>
-            </label>
-          </div>
-        <?php endif; ?>
-
-      <?php endif; ?>
 
       <div class="search-realm-advanced">
         <a href="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse', 'showAdvanced' => true, 'topLod' => false)) ?>">
