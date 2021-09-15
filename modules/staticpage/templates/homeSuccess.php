8bacf1d301c73839b7f7a0f7f55ce99f384d42a8
sborne <sylvain.borne@fr.ch>
2021-09-15 10:40:58 +0200

    Update homeSuccess.php
    cleaned code

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -36,7 +36,7 @@
 					'browsePhotographicArchives' => '/plugins/arBcuPlugin/images/archives_photo.png',
 					'browseFilmArchives' => '/plugins/arBcuPlugin/images/archives_film_son.png',
 					'browseInstitutionalArchives' => '/plugins/arBcuPlugin/images/archives_institutions.png',
-					//'browseFilmHeritage' => '/plugins/arBcuPlugin/images/patrimoine_film_son.png',
+					'browseFilmHeritage' => '/plugins/arBcuPlugin/images/patrimoine_film_son.png',
 					'browseManuscripts' => '/plugins/arBcuPlugin/images/livres_manuscrits.png',
 					'browseAncientPrints' => '/plugins/arBcuPlugin/images/imprimes_anciens_rares.png',
 					'browsePeriodicals' => '/plugins/arBcuPlugin/images/periodiques.png',
@@ -54,127 +54,13 @@
 								<img src="<?php echo ($thumbnails[$item->name]); ?>" alt="" href="#" class="">
 								<div class="bcu-overlay">
 									<div class="bcu-text">
-										<!-- Archives privées -->
 										<?php echo esc_entities($item->getLabel(['cultureFallback' => true])); ?>
-										<!-- <//?php echo image_tag($thumbnails[$item->name]); ?> -->
 									</div>
 								</div>
 							</a>
 						</li>
 					<?php } ?>
             	<?php } ?>
-				<!-- <li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/archives_musicales.png" alt="" href="#">
-						<div class="bcu-overlay">
-							<div class="bcu-text align-middle">Archives musicales</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/archives_photo.png" alt="" href="#">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Archives photographiques</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/archives_film_son.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Archives film et son</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/archives_institutions.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Archives d'institutions et manifestations culturelles</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/patrimoine_film_son.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Patrimoine film et son</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/livres_manuscrits.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Livres manuscrits</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/imprimes_anciens_rares.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Imprimés anciens et rares</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/periodiques.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Périodiques</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/monographie.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Monographies et ouvrages de référence</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/presse.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Presse</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/affiches_new.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Affiches</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/cartes_plans.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Cartes et plans</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/websites.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">Websites</div>
-						</div>
-					</a>
-				</li>
-				<li class="span2">
-					<a href="#" class="thumbnail bcu-thumbnail">
-						<img src="/plugins/arBcuPlugin/images/e-books.png" alt="">
-						<div class="bcu-overlay">
-							<div class="bcu-text">E-books</div>
-						</div>
-					</a>
-				</li> -->
 			</ul>
 		</div>
       </section>
