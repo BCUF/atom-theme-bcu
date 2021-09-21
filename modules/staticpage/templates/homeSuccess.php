d81df6c7343caee74a360c8f730e621d8ba86c89
sborne <sylvain.borne@fr.ch>
2021-09-21 15:28:45 +0200

    Update homeSuccess.php
    added ul tag to separate modules

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -19,21 +19,23 @@
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</ul>
-			<?php decorate_with('layout_1col.php') ?>
+			<ul>
+				<?php decorate_with('layout_1col.php') ?>
 
-			<?php slot('title') ?>
-			<input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit" value="<?php echo __('Reset') ?>" />
-			<input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit" data-label="<?php echo __('%1% more') ?>" value="" />
-			<?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
-			<h1><?php echo __('Hierarchy') ?></h1>
-			<?php end_slot() ?>
+				<?php slot('title') ?>
+					<input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit" value="<?php echo __('Reset') ?>" />
+					<input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit" data-label="<?php echo __('%1% more') ?>" value="" />
+					<?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
+					<h1><?php echo __('Hierarchy') ?></h1>
+				<?php end_slot() ?>
 
-			<?php slot('content') ?>
+				<?php slot('content') ?>
 
-			<div id='main-column'></div>
-			<span id="fullwidth-treeview-configuration" data-items-per-page="<?php echo $itemsPerPage ?>"></span>
+				<div id='main-column'></div>
+				<span id="fullwidth-treeview-configuration" data-items-per-page="<?php echo $itemsPerPage ?>"></span>
 
-			<?php end_slot() ?>
+				<?php end_slot() ?>
+			</ul>
 		</section>
 
 	<!-- <//?php echo get_component('default', 'popular', array('limit' => 10, 'sf_cache_key' => $sf_user->getCulture())) ?> -->
