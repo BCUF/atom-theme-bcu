181fb09dbf89ec9b41978ffbc61f2511b12bfd78
sborne <sylvain.borne@fr.ch>
2021-09-22 16:27:01 +0200

    moved code to the new file inside menu/staticpagemenu

--- templates/_header.php
+++ templates/_header.php
@@ -84,20 +84,6 @@
               <?php echo get_component('search', 'box') ?>
             </div>
 
-            <?php echo get_component('menu', 'staticPagesMenu') ?>
-        
-            <section>
-              <!-- <h2></?php echo __('Browse by') ?></h2> -->
-              <ul class="BCU-header-menu-ul">
-                <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID) ?>
-                <?php if ($browseMenu->hasChildren()): ?>
-                  <?php foreach ($browseMenu->getChildren() as $item): ?>
-                    <li class="BCU-header-menu-li"><a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>"><?php echo esc_specialchars($item->getLabel(array('cultureFallback' => true))) ?></a></li>
-                  <?php endforeach; ?>
-                <?php endif; ?>
-              </ul>
-            </section>
-
           </div>
         </div>
       </div>
