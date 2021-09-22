df1206d3085fb64d7bb76f3516f5b4160b12b3a2
sborne <sylvain.borne@fr.ch>
2021-09-22 13:51:49 +0200

    added class "bcu-input"

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -22,8 +22,8 @@
 				<!-- </?php decorate_with('layout_2col.php') ?> -->
 
 				<!-- </?php slot('title') ?> -->
-					<input style="display: none;" type="button" id="fullwidth-treeview-reset-button" class="" value="<?php echo __('Reset') ?>" />
-					<input style="display: none;"  type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit" data-label="<?php echo __('%1% more') ?>" value="" />
+					<input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit bcu-input" value="<?php echo __('Reset') ?>" />
+					<input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit bcu-input" data-label="<?php echo __('%1% more') ?>" value="" />
 					<?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
 					<h2><?php echo __('Hierarchy') ?></h2>
 				<!-- </?php end_slot() ?> -->
