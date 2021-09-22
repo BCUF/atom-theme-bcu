aaf058eaf9615ce41088f3c0f92b351617af5679
sborne <sylvain.borne@fr.ch>
2021-09-22 13:30:45 +0200

    rehabilitated button

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -19,12 +19,21 @@
 					<?php endforeach; ?>
 				<?php endif; ?>
 			</ul>
+				<!-- </?php decorate_with('layout_2col.php') ?> -->
 
+				<!-- </?php slot('title') ?> -->
+					<input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit" value="</?php echo __('Reset') ?>" />
+					<input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit" data-label="</?php echo __('%1% more') ?>" value="" />
+				<?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
 				<h2><?php echo __('Hierarchy') ?></h2>
+				<!-- </?php end_slot() ?> -->
+
+				<!-- </?php slot('content') ?> -->
 
 				<div id='main-column' class='span3'></div>
 				<span id="fullwidth-treeview-configuration" data-items-per-page="<?php echo $itemsPerPage ?>"></span>
 
+				<!-- </?php end_slot() ?> -->
 		</section>
 
 	<!-- <//?php echo get_component('default', 'popular', array('limit' => 10, 'sf_cache_key' => $sf_user->getCulture())) ?> -->
