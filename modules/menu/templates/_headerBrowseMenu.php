3fc24806964c0f4d7eed22bd83c46f9dd15a4f92
sborne <sylvain.borne@fr.ch>
2021-09-23 11:37:13 +0200

    Update _headerBrowseMenu.php
    adapted code to match actual variables

--- modules/menu/templates/_headerBrowseMenu.php
+++ modules/menu/templates/_headerBrowseMenu.php
@@ -1,6 +1,10 @@
-<h3><?php echo $Menu->getLabel() ?></h3>
-<?php foreach ($Menu->getChildren() as $item) : ?>
-  <a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>">
-    <?php echo $item->getLabel(array('cultureFallback' => true)) ?>
-  </a>
-<?php endforeach; ?>
+
+<h2><?php echo __('Browse by') ?></h2>
+<?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID) ?>
+<?php if ($browseMenu->hasChildren()): ?>
+  <?php foreach ($browseMenu->getChildren() as $item): ?>
+    <a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>">
+      <?php echo esc_specialchars($item->getLabel(array('cultureFallback' => true))) ?>
+    </a>
+  <?php endforeach; ?>
+<?php endif; ?>
