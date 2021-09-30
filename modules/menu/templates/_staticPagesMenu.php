96d2f872fad634b1b1ac1390453f22efd542df54
sborne <sylvain.borne@fr.ch>
2021-09-30 11:22:05 +0200

    added style to new icons

--- modules/menu/templates/_staticPagesMenu.php
+++ modules/menu/templates/_staticPagesMenu.php
@@ -1,13 +1,13 @@
 <?php $browseIcon = [
-  'browseInformationObjects' => '<i class="fa fa-map-marker" aria-hidden="true"></i>',
-  'browseSubjects' => '<i class="fa fa-tag" aria-hidden="true"></i>',
-  'browsePlaces' => '<i class="fa fa-map-marker" aria-hidden="true"></i>',
-  'browseActors' => '<i class="fa fa-users" aria-hidden="true"></i>', ]; ?>
+  'browseInformationObjects' => '<i class="bcu-browse-icon fa fa-tags" aria-hidden="true"></i>',
+  'browseSubjects' => '<i class="bcu-browse-icon fa fa-tag" aria-hidden="true"></i>',
+  'browsePlaces' => '<i class="bcu-browse-icon fa fa-map-marker" aria-hidden="true"></i>',
+  'browseActors' => '<i class="bcu-browse-icon fa fa-users" aria-hidden="true"></i>', ]; ?>
 
 <?php $browseMenu = QubitMenu::getByName('browse') ?>
 <?php if ($browseMenu->hasChildren()) { ?>
   <?php foreach ($browseMenu->getChildren() as $item) { ?>
-      <a class="bcu-browse-icon" href="<?php echo url_for($item->getPath(['getUrl' => true, 'resolveAlias' => true])); ?>" title="<?php echo esc_specialchars($item->getLabel(array('cultureFallback' => true))) ?>">
+      <a class="bcu-browse-link" href="<?php echo url_for($item->getPath(['getUrl' => true, 'resolveAlias' => true])); ?>" title="<?php echo esc_specialchars($item->getLabel(array('cultureFallback' => true))) ?>">
         <?php echo ($browseIcon[$item->name]); ?>
       </a>
   <?php } ?>
