a1c9ff88dd12330fbaf97719c418af176b211b6e
sborne <sylvain.borne@fr.ch>
2021-10-05 08:17:58 +0200

    removed div

--- modules/informationobject/templates/_contextMenu.php
+++ modules/informationobject/templates/_contextMenu.php
@@ -6,17 +6,15 @@
 
 <?php echo get_component('informationobject', 'treeView'); ?>
 
-<div>
-  <?php slot('contextMenuSidebar') ?>
-    <section>
+<?php slot('contextMenuSidebar') ?>
+  <section>
 
-        <input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit bcu-input" value="<?php echo __('Reset') ?>" />
-        <input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit bcu-input" data-label="<?php echo __('%1% more') ?>" value="" />
-        <?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
+      <input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit bcu-input" value="<?php echo __('Reset') ?>" />
+      <input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit bcu-input" data-label="<?php echo __('%1% more') ?>" value="" />
+      <?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
 
-      <div id='main-column' class='span3'></div>
-      <span id="fullwidth-treeview-configuration" data-items-per-page="<?php echo $itemsPerPage ?>"></span>
+    <div id='main-column' class='span3'></div>
+    <span id="fullwidth-treeview-configuration" data-items-per-page="<?php echo $itemsPerPage ?>"></span>
 
-    </section>
-  <?php end_slot() ?>
-</div>
+  </section>
+<?php end_slot() ?>
