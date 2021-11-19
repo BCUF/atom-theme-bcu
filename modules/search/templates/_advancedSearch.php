8ef54342788634542cf0c238c778d43640062d1f
sborne <sylvain.borne@fr.ch>
2021-11-19 14:22:00 +0100

    changed search help

--- modules/search/templates/_advancedSearch.php
+++ modules/search/templates/_advancedSearch.php
@@ -112,10 +112,13 @@
             </ul>
           </div>
         </div>
-        
-        <label>
-                <a href="#" class="date-range-help-icon" aria-expanded="false"><i class="fa fa-question-circle"></i></a>
-        </label>
+        <div class="filter-row">
+          <div class="lod-filter">
+            <label>
+              <a href="#" class="date-range-help-icon" aria-expanded="false"><i class="fa fa-question-circle"></i></a>
+            </label>
+          </div>
+        </div>
 
         <div class="alert alert-info date-range-help animateNicely">
             <?php echo __('<a href="https://atomqa.local.bcu-fribourg.ch/plugins/arBcuPlugin/assets/aide_recherche_atom_fr.pdf">Aide à la recherche</a>') ?>
