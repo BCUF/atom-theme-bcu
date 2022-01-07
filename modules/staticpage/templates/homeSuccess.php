7e5746492d0779d4e3a5a133b0624970629efdb1
sborne <sylvain.borne@fr.ch>
2022-01-07 08:43:58 +0100

    added indexSuccess.php and formatted code

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -1,23 +1,23 @@
 <div id="homepage-hero">
-    <?php decorate_with('layout_2col') ?>
+	<?php decorate_with('layout_2col') ?>
 
-    <?php slot('title') ?>
-      <h1><?php echo render_title($resource->getTitle(array('cultureFallback' => true))) ?></h1>
-    <?php end_slot() ?>
+	<?php slot('title') ?>
+	<h1><?php echo render_title($resource->getTitle(array('cultureFallback' => true))) ?></h1>
+	<?php end_slot() ?>
 
-    <?php slot('sidebar') ?>
+	<?php slot('sidebar') ?>
 
-		<section>
+	<section>
 
-				<input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit bcu-input" value="<?php echo __('Reset') ?>" />
-				<input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit bcu-input" data-label="<?php echo __('%1% more') ?>" value="" />
-				<?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
-				<h2><?php echo __('Hierarchy') ?></h2>
+		<input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit bcu-input" value="<?php echo __('Reset') ?>" />
+		<input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit bcu-input" data-label="<?php echo __('%1% more') ?>" value="" />
+		<?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
+		<h2><?php echo __('Hierarchy') ?></h2>
 
-			<div id='main-column' class='span3'></div>
-			<span id="fullwidth-treeview-configuration" data-items-per-page="<?php echo $itemsPerPage ?>"></span>
+		<div id='main-column' class='span3'></div>
+		<span id="fullwidth-treeview-configuration" data-items-per-page="<?php echo $itemsPerPage ?>"></span>
 
-		</section>
+	</section>
 
 	<?php end_slot() ?>
 
@@ -28,50 +28,51 @@
 		</section>
 
 		<section style="display: inline;">
-		<div>
-			<ul class="thumbnails bcu-thumbnails">
-				<?php $thumbnails = [
-					'browsePrivateArchives' => '/plugins/arBcuPlugin/images/archives_privees.png',
-					'browseMusicalArchives' => '/plugins/arBcuPlugin/images/archives_musicales.png',
-					'browsePhotographicArchives' => '/plugins/arBcuPlugin/images/archives_photo.png',
-					'browseFilmArchives' => '/plugins/arBcuPlugin/images/archives_film_son.png',
-					'browseInstitutionalArchives' => '/plugins/arBcuPlugin/images/archives_institutions.png',
-					'browseFilmHeritage' => '/plugins/arBcuPlugin/images/patrimoine_film_son.png',
-					'browseManuscripts' => '/plugins/arBcuPlugin/images/livres_manuscrits.png',
-					'browseAncientPrints' => '/plugins/arBcuPlugin/images/imprimes_anciens_rares.png',
-					'browsePeriodicals' => '/plugins/arBcuPlugin/images/periodiques.png',
-					'browseMonographs' => '/plugins/arBcuPlugin/images/monographie.png',
-					'browsePress' => '/plugins/arBcuPlugin/images/presse.png',
-					'browsePosters' => '/plugins/arBcuPlugin/images/affiches_new.png',
-					'browseMaps' => '/plugins/arBcuPlugin/images/cartes_plans.png',
-					'browseWebsites' => '/plugins/arBcuPlugin/images/websites.png',
-					'browseEBooks' => '/plugins/arBcuPlugin/images/e-books.png', ]; ?>
-				<?php $MainNav = QubitMenu::getByName('MainNav'); ?>
-				<?php if ($MainNav->hasChildren()) { ?>
-					<?php foreach ($MainNav->getChildren() as $item) { ?>
-						<li class="span2">
-							<a href="<?php echo url_for($item->getPath(['getUrl' => true, 'resolveAlias' => true])); ?>" class="thumbnail bcu-thumbnail">
-								<img src="<?php echo ($thumbnails[$item->name]); ?>" alt="" href="#" class="">
-								<div class="bcu-overlay">
-									<div class="bcu-text">
-										<?php echo esc_entities($item->getLabel(['cultureFallback' => true])); ?>
+			<div>
+				<ul class="thumbnails bcu-thumbnails">
+					<?php $thumbnails = [
+						'browsePrivateArchives' => '/plugins/arBcuPlugin/images/archives_privees.png',
+						'browseMusicalArchives' => '/plugins/arBcuPlugin/images/archives_musicales.png',
+						'browsePhotographicArchives' => '/plugins/arBcuPlugin/images/archives_photo.png',
+						'browseFilmArchives' => '/plugins/arBcuPlugin/images/archives_film_son.png',
+						'browseInstitutionalArchives' => '/plugins/arBcuPlugin/images/archives_institutions.png',
+						'browseFilmHeritage' => '/plugins/arBcuPlugin/images/patrimoine_film_son.png',
+						'browseManuscripts' => '/plugins/arBcuPlugin/images/livres_manuscrits.png',
+						'browseAncientPrints' => '/plugins/arBcuPlugin/images/imprimes_anciens_rares.png',
+						'browsePeriodicals' => '/plugins/arBcuPlugin/images/periodiques.png',
+						'browseMonographs' => '/plugins/arBcuPlugin/images/monographie.png',
+						'browsePress' => '/plugins/arBcuPlugin/images/presse.png',
+						'browsePosters' => '/plugins/arBcuPlugin/images/affiches_new.png',
+						'browseMaps' => '/plugins/arBcuPlugin/images/cartes_plans.png',
+						'browseWebsites' => '/plugins/arBcuPlugin/images/websites.png',
+						'browseEBooks' => '/plugins/arBcuPlugin/images/e-books.png',
+					]; ?>
+					<?php $MainNav = QubitMenu::getByName('MainNav'); ?>
+					<?php if ($MainNav->hasChildren()) { ?>
+						<?php foreach ($MainNav->getChildren() as $item) { ?>
+							<li class="span2">
+								<a href="<?php echo url_for($item->getPath(['getUrl' => true, 'resolveAlias' => true])); ?>" class="thumbnail bcu-thumbnail">
+									<img src="<?php echo ($thumbnails[$item->name]); ?>" alt="" href="#" class="">
+									<div class="bcu-overlay">
+										<div class="bcu-text">
+											<?php echo esc_entities($item->getLabel(['cultureFallback' => true])); ?>
+										</div>
 									</div>
-								</div>
-							</a>
-						</li>
+								</a>
+							</li>
+						<?php } ?>
 					<?php } ?>
-				<?php } ?>
-			</ul>
-		</div>
+				</ul>
+			</div>
 		</section>
 
-		<?php if (QubitAcl::check($resource, 'update')): ?>
+		<?php if (QubitAcl::check($resource, 'update')) : ?>
 			<section class="actions">
 				<ul>
-				<li><?php echo link_to(__('Edit'), array($resource, 'module' => 'staticpage', 'action' => 'edit'), array('title' => __('Edit this page'), 'class' => 'c-btn')) ?></li>
+					<li><?php echo link_to(__('Edit'), array($resource, 'module' => 'staticpage', 'action' => 'edit'), array('title' => __('Edit this page'), 'class' => 'c-btn')) ?></li>
 				</ul>
 			</section>
-		<?php endif; ?>      
+		<?php endif; ?>
 	</div>
 
 
---
+++ modules/staticpage/templates/indexSuccess.php
@@ -0,0 +1,44 @@
+<?php $menu = get_component('menu', 'staticPagesMenu') ?>
+<?php $layout = 'layout_1col' ?>
+<?php if (!empty($menu)) : ?>
+    <?php $layout = 'layout_2col' ?>
+    <?php slot('sidebar') ?>
+    <section>
+
+        <input type="button" id="fullwidth-treeview-reset-button" class="c-btn c-btn-submit bcu-input" value="<?php echo __('Reset') ?>" />
+        <input type="button" id="fullwidth-treeview-more-button" class="c-btn c-btn-submit bcu-input" data-label="<?php echo __('%1% more') ?>" value="" />
+        <?php echo image_tag('/vendor/jstree/themes/default/throbber.gif', array('id' => 'fullwidth-treeview-activity-indicator', 'alt' => __('Loading ...'))) ?>
+        <h2><?php echo __('Hierarchy') ?></h2>
+
+        <div id='main-column' class='span3'></div>
+        <span id="fullwidth-treeview-configuration" data-items-per-page="<?php echo $itemsPerPage ?>"></span>
+
+    </section>
+    <?php end_slot() ?>
+<?php endif; ?>
+<?php decorate_with($layout) ?>
+
+<?php slot('title') ?>
+<h1><?php echo render_title($resource->getTitle(array('cultureFallback' => true))) ?></h1>
+<?php end_slot() ?>
+
+<div class="page">
+
+    <div>
+        <?php echo render_value_html($sf_data->getRaw('content')) ?>
+    </div>
+
+</div>
+
+<?php if (QubitAcl::check($resource, 'update')) : ?>
+    <?php slot('after-content') ?>
+    <section class="actions">
+        <ul>
+            <li><?php echo link_to(__('Edit'), array($resource, 'module' => 'staticpage', 'action' => 'edit'), array('class' => 'c-btn c-btn-submit', 'title' => __('Edit this page'))) ?></li>
+            <?php if (QubitAcl::check($resource, 'delete')) : ?>
+                <li><?php echo link_to(__('Delete'), array($resource, 'module' => 'staticpage', 'action' => 'delete'), array('class' => 'c-btn c-btn-delete')) ?></li>
+            <?php endif; ?>
+        </ul>
+    </section>
+    <?php end_slot() ?>
+<?php endif; ?>
