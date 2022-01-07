b202b70012369d7048c909cfbdc0a0683ec867a1
sborne <sylvain.borne@fr.ch>
2022-01-07 11:14:32 +0100

    reverted to last

--- modules/staticpage/actions/indexAction.class.php
+++ modules/staticpage/actions/indexAction.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class StaticPageIndexAction extends sfAction
+class StaticPageHomeAction extends StaticPageIndexAction
 {
     public function execute($request)
     {
@@ -12,4 +12,51 @@ class StaticPageIndexAction extends sfAction
 
         $this->itemsPerPage = sfConfig::get('app_treeview_full_items_per_page', 50);
     }
+}
+
+class StaticPageIndexAction extends sfAction
+{
+  public function execute($request)
+  {
+    $this->resource = $this->getRoute()->resource;
+
+    if (1 > strlen($title = $this->resource->__toString()))
+    {
+      $title = $this->context->i18n->__('Untitled');
+    }
+
+    $this->response->setTitle("$title - {$this->response->getTitle()}");
+
+    $this->content = $this->getPurifiedStaticPageContent();
+
+    if (sfConfig::get('app_enable_institutional_scoping') && $this->resource->slug == 'home')
+    {
+      // Remove the search-realm attribute
+      $this->context->user->removeAttribute('search-realm');
+    }
+  }
+
+  protected function getPurifiedStaticPageContent()
+  {
+    $culture = sfContext::getInstance()->getUser()->getCulture();
+    $cacheKey = 'staticpage:'.$this->resource->id.':'.$culture;
+    $cache = QubitCache::getInstance();
+
+    if (null === $cache)
+    {
+      return;
+    }
+
+    if ($cache->has($cacheKey))
+    {
+      return $cache->get($cacheKey);
+    }
+
+    $content = $this->resource->getContent(array('cultureFallback' => true));
+    $content = QubitHtmlPurifier::getInstance()->purify($content);
+
+    $cache->set($cacheKey, $content);
+
+    return $content;
+  }
 }
