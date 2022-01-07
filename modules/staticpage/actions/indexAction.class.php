cdb2d8e4f7582e60d51c62a4439b3c252d8bcafb
sborne <sylvain.borne@fr.ch>
2022-01-07 10:04:04 +0100

    Update indexAction.class.php

--- modules/staticpage/actions/indexAction.class.php
+++ modules/staticpage/actions/indexAction.class.php
@@ -1,10 +1,10 @@
 <?php
 
-class StaticPageHomeAction extends StaticPageIndexAction
+class StaticPageIndexAction extends sfAction
 {
     public function execute($request)
     {
-	parent::execute($request);
+        parent::execute($request);
         // Check user authorization
         if ('no' === sfConfig::get('app_treeview_show_browse_hierarchy_page', 'no')) {
             QubitAcl::forwardUnauthorized();
@@ -12,51 +12,43 @@ class StaticPageHomeAction extends StaticPageIndexAction
 
         $this->itemsPerPage = sfConfig::get('app_treeview_full_items_per_page', 50);
     }
-}
-
-class StaticPageIndexAction extends sfAction
-{
-  public function execute($request)
-  {
-    $this->resource = $this->getRoute()->resource;
-
-    if (1 > strlen($title = $this->resource->__toString()))
+    public function execute($request)
     {
-      $title = $this->context->i18n->__('Untitled');
+        $this->resource = $this->getRoute()->resource;
+
+        if (1 > strlen($title = $this->resource->__toString())) {
+            $title = $this->context->i18n->__('Untitled');
+        }
+
+        $this->response->setTitle("$title - {$this->response->getTitle()}");
+
+        $this->content = $this->getPurifiedStaticPageContent();
+
+        if (sfConfig::get('app_enable_institutional_scoping') && $this->resource->slug == 'home') {
+            // Remove the search-realm attribute
+            $this->context->user->removeAttribute('search-realm');
+        }
     }
 
-    $this->response->setTitle("$title - {$this->response->getTitle()}");
-
-    $this->content = $this->getPurifiedStaticPageContent();
-
-    if (sfConfig::get('app_enable_institutional_scoping') && $this->resource->slug == 'home')
+    protected function getPurifiedStaticPageContent()
     {
-      // Remove the search-realm attribute
-      $this->context->user->removeAttribute('search-realm');
+        $culture = sfContext::getInstance()->getUser()->getCulture();
+        $cacheKey = 'staticpage:' . $this->resource->id . ':' . $culture;
+        $cache = QubitCache::getInstance();
+
+        if (null === $cache) {
+            return;
+        }
+
+        if ($cache->has($cacheKey)) {
+            return $cache->get($cacheKey);
+        }
+
+        $content = $this->resource->getContent(array('cultureFallback' => true));
+        $content = QubitHtmlPurifier::getInstance()->purify($content);
+
+        $cache->set($cacheKey, $content);
+
+        return $content;
     }
-  }
-
-  protected function getPurifiedStaticPageContent()
-  {
-    $culture = sfContext::getInstance()->getUser()->getCulture();
-    $cacheKey = 'staticpage:'.$this->resource->id.':'.$culture;
-    $cache = QubitCache::getInstance();
-
-    if (null === $cache)
-    {
-      return;
-    }
-
-    if ($cache->has($cacheKey))
-    {
-      return $cache->get($cacheKey);
-    }
-
-    $content = $this->resource->getContent(array('cultureFallback' => true));
-    $content = QubitHtmlPurifier::getInstance()->purify($content);
-
-    $cache->set($cacheKey, $content);
-
-    return $content;
-  }
 }
