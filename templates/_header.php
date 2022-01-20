59d535047819d1850fc521fdc62f47cf15cf86bd
sborne <sylvain.borne@fr.ch>
2022-01-20 08:09:37 +0100

    changed logo

--- templates/_header.php
+++ templates/_header.php
@@ -2,14 +2,14 @@
 
 <?php echo get_component('default', 'updateCheck') ?>
 
-<?php if ($sf_user->isAuthenticated()): ?>
+<?php if ($sf_user->isAuthenticated()) : ?>
   <div id="top-bar">
     <nav>
       <?php echo get_component('menu', 'userMenu') ?>
-      <?php if (sfConfig::get('app_toggleLanguageMenu')): ?>
+      <?php if (sfConfig::get('app_toggleLanguageMenu')) : ?>
         <?php echo get_component('menu', 'changeLanguageMenu') ?>
       <?php endif; ?>
-      <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+      <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture() . $sf_user->getUserID())) ?>
     </nav>
   </div>
 <?php endif; ?>
@@ -21,92 +21,95 @@
     <div id="header-lvl1">
       <div class="row">
 
-        <div id="logo-and-name" class="span3" >
-          <?php if ('fr' == $sf_user->getCulture()): ?>
+        <div id="logo-and-name" class="span3">
+          <?php if ('fr' == $sf_user->getCulture()) : ?>
             <h1><?php echo link_to(image_tag('/plugins/arBcuPlugin/images/header-logo-bcu.svg', array('alt' => __('Bibliothèque cantonale et universitaire'))), 'https://www.fr.ch/bcufr', array('rel' => 'home')) ?></h1>
-          <?php else: ?>
+          <?php else : ?>
             <h1><?php echo link_to(image_tag('/plugins/arBcuPlugin/images/header-logo-bcu.svg', array('alt' => __('Kantons-und Universitätsbibliothek'))), 'https://www.fr.ch/kub', array('rel' => 'home')) ?></h1>
           <?php endif; ?>
         </div>
 
-        <!-- <div id="header-title" class="span4">
-           <h1><--?php echo __('Fri-Memoria') ?></h1>
-	      </div> -->
-        <div id="Fri-Memoria-logo" class="span4"style="padding-left: 20px;">
-          <h1><img src="/plugins/arBcuPlugin/images/FriMemoria-logo.svg"></h1>
-        </div>
+        <?php if ('fr' == $sf_user->getCulture()) : ?>
+          <div id="Fri-Memoria-logo" class="span4" style="padding-left: 20px;">
+            <h1><img src="/plugins/arBcuPlugin/images/FriMemoria-logo_new.svg"></h1>
+          </div>
+        <?php else : ?>
+          <div id="Fri-Memoria-logo" class="span4" style="padding-left: 20px;">
+            <h1><img src="/plugins/arBcuPlugin/images/FriMemoria-logo_new_DE.svg"></h1>
+          </div>
+        <?php endif; ?>
 
         <div id="header-nav-container" class="span5">
 
 
           <ul style id="header-nav" class="nav nav-pills pull-right">
 
-            <?php if ('fr' == $sf_user->getCulture()): ?>
+            <?php if ('fr' == $sf_user->getCulture()) : ?>
               <li><?php echo link_to(__('Home'), '/index.php?sf_culture=fr') ?></li>
-            <?php else: ?>
+            <?php else : ?>
               <li><?php echo link_to(__('Home'), '/index.php?sf_culture=de') ?></li>
             <?php endif; ?>
 
-            <?php if ('fr' == $sf_user->getCulture()): ?>
+            <?php if ('fr' == $sf_user->getCulture()) : ?>
               <li><?php echo link_to(__('Contactez-nous'), 'https://www.fr.ch/bcu/contact') ?></li>
-            <?php else: ?>
+            <?php else : ?>
               <li><?php echo link_to(__('Kontaktieren Sie uns'), 'https://www.fr.ch/de/kub/kontakt') ?></li>
             <?php endif; ?>
 
-            <?php foreach (array('fr', 'de') as $item): ?>
-              <?php if ($sf_user->getCulture() != $item): ?>
+            <?php foreach (array('fr', 'de') as $item) : ?>
+              <?php if ($sf_user->getCulture() != $item) : ?>
                 <li><?php echo link_to(format_language($item, $item), array('sf_culture' => $item) + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()) ?></li>
                 <?php break; ?>
               <?php endif; ?>
             <?php endforeach; ?>
 
             <?php
-              $local_url = "local.bcu-fribourg.ch";
-              // $local_url = "localhost";
-              $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+            $local_url = "local.bcu-fribourg.ch";
+            // $local_url = "localhost";
+            $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-              if ((!$sf_user->isAuthenticated())&&(strpos($actual_link, $local_url) !== false)){
-                echo "<li>" . link_to(__('Log in'), array('module' => 'user', 'action' => 'login')) . "</li>";
-              }
-              
+            if ((!$sf_user->isAuthenticated()) && (strpos($actual_link, $local_url) !== false)) {
+              echo "<li>" . link_to(__('Log in'), array('module' => 'user', 'action' => 'login')) . "</li>";
+            }
+
             ?>
 
           </ul>
-          
+
         </div>
       </div>
       <div class="row">
-          <div class="span6" style="vertical-align: top;">
-            <?php echo get_component('menu', 'staticPagesMenu') ?>
+        <div class="span6" style="vertical-align: top;">
+          <?php echo get_component('menu', 'staticPagesMenu') ?>
 
-            <?php if ('fr' == $sf_user->getCulture()): ?>
-              <span title="Presse-papier" style="padding-left: 70px;"><?php echo get_component('menu', 'clipboardMenu') ?></span>
-            <?php else: ?>
-              <span title="Zwischenablage" style="padding-left: 70px;"><?php echo get_component('menu', 'clipboardMenu') ?></span>
-            <?php endif; ?>
+          <?php if ('fr' == $sf_user->getCulture()) : ?>
+            <span title="Presse-papier" style="padding-left: 70px;"><?php echo get_component('menu', 'clipboardMenu') ?></span>
+          <?php else : ?>
+            <span title="Zwischenablage" style="padding-left: 70px;"><?php echo get_component('menu', 'clipboardMenu') ?></span>
+          <?php endif; ?>
 
-            <?php if ('fr' == $sf_user->getCulture()): ?>
-              <span title="Liens rapides"><?php echo get_component('menu', 'quickLinksMenu') ?></span>
-            <?php else: ?>
-              <span title="Direkter Link"><?php echo get_component('menu', 'quickLinksMenu') ?></span>
-            <?php endif; ?>
-            
-          </div>
-          <span class="span1"></span>
-          <div id="header-search" class="span5">
-            <?php echo get_component('search', 'box') ?>
-          </div>
-          
-          <!-- <div id="header-lvl2">
+          <?php if ('fr' == $sf_user->getCulture()) : ?>
+            <span title="Liens rapides"><?php echo get_component('menu', 'quickLinksMenu') ?></span>
+          <?php else : ?>
+            <span title="Direkter Link"><?php echo get_component('menu', 'quickLinksMenu') ?></span>
+          <?php endif; ?>
+
+        </div>
+        <span class="span1"></span>
+        <div id="header-search" class="span5">
+          <?php echo get_component('search', 'box') ?>
+        </div>
+
+        <!-- <div id="header-lvl2">
             <div class="row"> -->
 
-            <!-- </div>
+        <!-- </div>
           </div> -->
-        </div>
       </div>
     </div>
-
-
   </div>
+
+
+</div>
 
 </div>
