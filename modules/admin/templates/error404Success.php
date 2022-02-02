0408c75ad097e2054374db45bf385b99d6f62656
sborne <sylvain.borne@fr.ch>
2022-02-02 14:08:59 +0100

    Update error404Success.php

--- modules/admin/templates/error404Success.php
+++ modules/admin/templates/error404Success.php
@@ -1,20 +1,24 @@
 <section class="admin-message" id="error-404">
 
-  <h2>
-    <i class="fa fa-times"></i>
-    <?php echo __('Sorry, page not found'); ?>
-  </h2>
+    <h2>
+        <!-- <i class="fa fa-times"></i> -->
+        <?php if ('fr' == $sf_user->getCulture()) : ?>
+            <li><?php echo '<h3>Ce document est accessible sur le poste de consultation des Archives électroniques à la BCU-Centrale.</h3>' ?></li>
+        <?php else : ?>
+            <li><?php echo '<h3>Dieses Dokument ist an der Station der elektronische Archivierung in der KUB-Zentrale zu konsultieren.</h3>' ?></li>
+        <?php endif; ?>
+    </h2>
 
-  <p>
-    <?php echo __('Did you type the URL correctly?'); ?><br />
-    <?php echo __('Did you follow a broken link blablabla?'); ?>
-  </p>
-
-  <div class="tips">
     <p>
-      <a href="javascript:history.go(-1)"><?php echo __('Back to previous page'); ?></a><br />
-      <?php echo link_to(__('Go to homepage'), '@homepage'); ?>
+        <?php echo __('Did you type the URL correctly?'); ?><br />
+        <?php echo __('Did you follow a broken link blablabla?'); ?>
     </p>
-  </div>
+
+    <div class="tips">
+        <p>
+            <a href="javascript:history.go(-1)"><?php echo __('Back to previous page'); ?></a><br />
+            <?php echo link_to(__('Go to homepage'), '@homepage'); ?>
+        </p>
+    </div>
 
 </section>
