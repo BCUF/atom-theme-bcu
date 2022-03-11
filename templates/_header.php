61fd91bc3240975a1f1778177f76a044805dc601
sborne <sylvain.borne@fr.ch>
2022-03-11 14:19:40 +0100

    changed feedback

--- templates/_header.php
+++ templates/_header.php
@@ -42,7 +42,7 @@
         <?php if ('fr' == $sf_user->getCulture()) : ?>
           <a id="feedback-link" href="#" target="blank" title="Votre avis nous intéresse !"><img id="feedback-img" src="/plugins/arBcuPlugin/images/feedbackIconRight.svg"></img></a>
         <?php else : ?>
-          <a id="feedback-link" href="#" target="blank" title="Ihre Meinung ist gefragt !"><img id="feedback-img" src="/plugins/arBcuPlugin/images/feedbackIconRightDE.svg"></img></a>
+          <a id="feedback-link" href="#" target="blank" title="Ihre Meinung ist uns wichtig !"><img id="feedback-img" src="/plugins/arBcuPlugin/images/feedbackIconRightDE.svg"></img></a>
         <?php endif; ?>
 
         <div id="header-nav-container" class="span5">
