98d03c5e8effdf7a30da78d16fe5f406d30ade04
nstulz <nicolas.stulz@fr.ch>
2025-02-24 12:38:08 +0100

    BS5 install ok

--- config/arBcuPluginConfiguration.class.php
+++ config/arBcuPluginConfiguration.class.php
@@ -1,43 +1,46 @@
 <?php
 
-class arBcuPluginConfiguration extends sfPluginConfiguration
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once sfConfig::get('sf_plugins_dir')
+    .'/arDominionB5Plugin/config/arDominionB5PluginConfiguration.class.php';
+
+class arBcuPluginConfiguration extends arDominionB5PluginConfiguration
 {
-  // Summary and version. AtoM recognizes any plugin as a theme as long as
-  // the $summary string contains the word "theme" in it (case-insensitive).
-  public static
-    $summary = 'Theme plugin, extension of arDominionPlugin.',
-    $version = '0.0.1';
+    public static $summary = 'BCU Fribourg Custom B5 theme plugin, extension of arDominionB5Plugin without modifications.';
+    public static $version = '0.0.1';
 
-  public function contextLoadFactories(sfEvent $event)
-  {
-    // Here we are including the CSS stylesheet build in our pages.
-    $context = $event->getSubject();
-    $context->response->addStylesheet('/plugins/arBcuPlugin/css/min.css', 'last', array('media' => 'all'));
-    //add custom js for read-more/less
-    $context->response->addJavaScript('/plugins/arBcuPlugin/js/bcuCustom.js', 'last');
-  }
+    public function initialize()
+    {
+        parent::initialize();
 
-  public function initialize()
-  {
-    
-    
-    // Run the class method contextLoadFactories defined above once Symfony
-    // is done loading the internal framework factories.
-    $this->dispatcher->connect('context.load_factories', array($this, 'contextLoadFactories'));
+        // Add this plugin templates before arDominionB5Plugin
+        sfConfig::set('sf_decorator_dirs', array_merge(
+            [$this->rootDir.'/templates'],
+            sfConfig::get('sf_decorator_dirs')
+        ));
 
-    // This allows us to override the application decorators.
-    $decoratorDirs = sfConfig::get('sf_decorator_dirs');
-    $decoratorDirs[] = $this->rootDir.'/templates';
-    sfConfig::set('sf_decorator_dirs', $decoratorDirs);
-
-    // This allows us to override the contents of the application modules.
-    $moduleDirs = sfConfig::get('sf_module_dirs');
-    $moduleDirs[$this->rootDir.'/modules'] = false;
-    sfConfig::set('sf_module_dirs', $moduleDirs);
- 
-    // Custom OAI set definitions
-    QubitOai::addOaiSet(new QubitOaiBcuSet());
-    QubitOai::addOaiSet(new QubitOaiFondsRFrSet());
-    
-  }
+        // Move this plugin to the top to allow overwriting
+        // controllers and views from other plugin modules.
+        $plugins = $this->configuration->getPlugins();
+        if (false !== $key = array_search($this->name, $plugins)) {
+            unset($plugins[$key]);
+        }
+        $this->configuration->setPlugins(array_merge([$this->name], $plugins));
+    }
 }
