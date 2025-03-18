39d6085faef0ba6af6ffa55b208bd61edbcfc5ba
nstulz <nicolas.stulz@fr.ch>
2025-03-18 14:23:21 +0100

    fix plugins image and added test images

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -69,72 +69,31 @@
 <?php end_slot(); ?>
 
 <?php slot('main-nav'); ?>
-
-  <!-- ?php $thumbnails = [
-		'browsePrivateArchives' => '/plugins/arBcuPlugin/images/archives_privees.png',
-		'browseMusicalArchives' => '/plugins/arBcuPlugin/images/archives_musicales.png',
-		'browsePhotographicArchives' => '/plugins/arBcuPlugin/images/archives_photo.png',
-		'browseFilmArchives' => '/plugins/arBcuPlugin/images/archives_film_son.png',
-		'browseInstitutionalArchives' => '/plugins/arBcuPlugin/images/archives_institutions.png',
-		'browseFilmHeritage' => '/plugins/arBcuPlugin/images/patrimoine_film_son.png',
-		'browseManuscripts' => '/plugins/arBcuPlugin/images/livres_manuscrits.png',
-		'browseAncientPrints' => '/plugins/arBcuPlugin/images/imprimes_anciens_rares.png',
-		'browsePeriodicals' => '/plugins/arBcuPlugin/images/periodiques.png',
-		'browseMonographs' => '/plugins/arBcuPlugin/images/monographie.png',
-		'browsePress' => '/plugins/arBcuPlugin/images/presse.png',
-		'browsePosters' => '/plugins/arBcuPlugin/images/affiches_new.png',
-		'browseMaps' => '/plugins/arBcuPlugin/images/cartes_plans.png',
-		'browseWebsites' => '/plugins/arBcuPlugin/images/websites.png',
-		'browseEBooks' => '/plugins/arBcuPlugin/images/e-books.png',
-		'browseBibliography' => '/plugins/arBcuPlugin/images/bibliographie_fribourgeoise.png',
-	]; ? -->
-
 <?php 
 
 $MainNav = QubitMenu::getByName('MainNav');
 if (isset($MainNav) && $MainNav->hasChildren()) {
 
-  // $thumbnails = [
-  //   'thumbnail01' => '/plugins/arBcuPlugin/images/thumbnail01.jpg',
-  //   'thumbnail02' => '/plugins/arBcuPlugin/images/thumbnail02.jpg',
-  //   'thumbnail03' => '/plugins/arBcuPlugin/images/thumbnail03.jpg',
-  //   'thumbnail04' => '/plugins/arBcuPlugin/images/thumbnail04.jpg',
-  //   'thumbnail05' => '/plugins/arBcuPlugin/images/thumbnail05.jpg',
-  //   'thumbnail06' => '/plugins/arBcuPlugin/images/thumbnail06.jpg',
-  //   'thumbnail07' => '/plugins/arBcuPlugin/images/thumbnail07.jpg',
-  //   'thumbnail08' => '/plugins/arBcuPlugin/images/thumbnail08.jpg',
-  //   'thumbnail09' => '/plugins/arBcuPlugin/images/thumbnail09.jpg',
-  //   'thumbnail10' => '/plugins/arBcuPlugin/images/thumbnail10.jpg',
-  //   'thumbnail11' => '/plugins/arBcuPlugin/images/thumbnail11.jpg',
-  //   'thumbnail12' => '/plugins/arBcuPlugin/images/thumbnail12.jpg',
-  //   'thumbnail13' => '/plugins/arBcuPlugin/images/thumbnail13.jpg',
-  //   'thumbnail14' => '/plugins/arBcuPlugin/images/thumbnail14.jpg',
-  //   'thumbnail15' => '/plugins/arBcuPlugin/images/thumbnail15.jpg',
-  //   'thumbnail16' => '/plugins/arBcuPlugin/images/thumbnail16.jpg',
-  //   'thumbnail17' => '/plugins/arBcuPlugin/images/thumbnail17.jpg',
-  //   'thumbnail18' => '/plugins/arBcuPlugin/images/thumbnail18.jpg'
-  // ]; 
-
   $thumbnails = [
-    'thumbnail01' => '/images/thumbnail01.jpg',
-    'thumbnail02' => '/images/thumbnail02.jpg',
-    'thumbnail03' => '/images/thumbnail03.jpg',
-    'thumbnail04' => '/images/thumbnail04.jpg',
-    'thumbnail05' => '/images/thumbnail05.jpg',
-    'thumbnail06' => '/images/thumbnail06.jpg',
-    'thumbnail07' => '/images/thumbnail07.jpg',
-    'thumbnail08' => '/images/thumbnail08.jpg',
-    'thumbnail09' => '/images/thumbnail09.jpg',
-    'thumbnail10' => '/images/thumbnail10.jpg',
-    'thumbnail11' => '/images/thumbnail11.jpg',
-    'thumbnail12' => '/images/thumbnail12.jpg',
-    'thumbnail13' => '/images/thumbnail13.jpg',
-    'thumbnail14' => '/images/thumbnail14.jpg',
-    'thumbnail15' => '/images/thumbnail15.jpg',
-    'thumbnail16' => '/images/thumbnail16.jpg',
-    'thumbnail17' => '/images/thumbnail17.jpg',
-    'thumbnail18' => '/images/thumbnail18.jpg'
-  ]; 
+    'thumbnail01' => '/plugins/arBcuPlugin/images/thumbnail01.jpg',
+    'thumbnail02' => '/plugins/arBcuPlugin/images/thumbnail02.jpg',
+    'thumbnail03' => '/plugins/arBcuPlugin/images/thumbnail03.jpg',
+    'thumbnail04' => '/plugins/arBcuPlugin/images/thumbnail04.jpg',
+    'thumbnail05' => '/plugins/arBcuPlugin/images/thumbnail05.jpg',
+    'thumbnail06' => '/plugins/arBcuPlugin/images/thumbnail06.jpg',
+    'thumbnail07' => '/plugins/arBcuPlugin/images/thumbnail07.jpg',
+    'thumbnail08' => '/plugins/arBcuPlugin/images/thumbnail08.jpg',
+    'thumbnail09' => '/plugins/arBcuPlugin/images/thumbnail09.jpg',
+    'thumbnail10' => '/plugins/arBcuPlugin/images/thumbnail10.jpg',
+    'thumbnail11' => '/plugins/arBcuPlugin/images/thumbnail11.jpg',
+    'thumbnail12' => '/plugins/arBcuPlugin/images/thumbnail12.jpg',
+    'thumbnail13' => '/plugins/arBcuPlugin/images/thumbnail13.jpg',
+    'thumbnail14' => '/plugins/arBcuPlugin/images/thumbnail14.jpg',
+    'thumbnail15' => '/plugins/arBcuPlugin/images/thumbnail15.jpg',
+    'thumbnail16' => '/plugins/arBcuPlugin/images/thumbnail16.jpg',
+    'thumbnail17' => '/plugins/arBcuPlugin/images/thumbnail17.jpg',
+    'thumbnail18' => '/plugins/arBcuPlugin/images/thumbnail18.jpg'
+  ];
 ?>
 
 		<?php foreach ($MainNav->getChildren() as $item) { ?>
